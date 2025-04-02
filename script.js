e6d6635d33060c480fe8fe79e3ee718189fa1cc4
SatyajitB2006 <satyajitbhisekar9@gmail.com>
2025-04-02 14:01:46 -0700

    Add files via upload
    yes

---
+++ script.js
@@ -0,0 +1,108 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click
+());
+
+
+
+
+
+
+
+
+
+// 1111111 English
+
+let change = document.querySelector("#mylink1");
+let subtitle = document.querySelector(".subtitle");
+let description = document.querySelector(".description");
+let btn = document.querySelector("#Home");
+let btn2 = document.querySelector("#Dashboard"); 
+let btn3 = document.querySelector("#pc");
+let btn4 = document.querySelector("#Schemes");
+let btn5 = document.querySelector("#dd");
+let btn6 = document.querySelector("#Grievance");
+let btn7 = document.querySelector("Login");
+let btn8 = document.querySelector("#SignUp");
+ 
+
+function change1 ()  {
+    mylink1.onclick = () => {
+        subtitle.innerText = "Unleashing Potential, One Athlete at a Time";
+        description.innerText = "**Athlence** is a smart athlete management platform that connects athletes, coaches, and sponsors. It helps track performance, manage careers, and unlock new opportunities in sports. With cutting-edge technology, we empower athletes to achieve their full potential.";
+        Home.innerText = "Home";
+        Dashboard.innerText = "Dashboard";
+        pc.innerText = "Personal Coaches";
+        Schemes.innerText = "Schemes";
+        dd.innerText = "Collaboration";
+        Grievance.innerText = "Grievance";
+        Login.innerText = "Login";
+        SignUp.innerText = "Sign Up";
+    }
+} 
+
+//2222222 hindi 
+
+let hindi = document.querySelector("#mylink2");
+let hindisubtitle = document.querySelector(".subtitle");
+let hindidescription = document.querySelector(".description");
+let hbtn = document.querySelector("#Home");
+let hbtn2 = document.querySelector("#Dashboard"); 
+let hbtn3 = document.querySelector("#pc");
+let hbtn4 = document.querySelector("#Schemes");
+let hbtn5 = document.querySelector("#dd");
+let hbtn6 = document.querySelector("#Grievance");
+let hbtn7 = document.querySelector("Login");
+let hbtn8 = document.querySelector("#Sign Up");
+
+
+
+function change2 ()  {
+    mylink2.onclick = () => {
+        subtitle.innerText = "संभावनाओं की उड़ान, हर एथलीट के साथ";
+        hindidescription.innerText = "Athlence एक स्मार्ट एथलीट प्रबंधन प्लेटफ़ॉर्म है जो एथलीट्स, कोच और प्रायोजकों को जोड़ता है | यह प्रदर्शन को ट्रैक करने, करियर प्रबंधित करने और खेल जगत में नए अवसरों को खोलने में मदद करता है। आधुनिक तकनीक के साथ, हम एथलीट्स को उनकी पूर्ण क्षमता प्राप्त करने के लिए सशक्त बनाते हैं।"
+        Home.innerText = "मुख्य पृष्ठ";
+        Dashboard.innerText = "नियंत्रण पैनल";
+        pc.innerText = "व्यक्तिगत प्रशिक्षक";
+        Schemes.innerText = "योजनाएँ";
+        dd.innerText = "सहयोग";
+        Grievance.innerText = "शिकायत";
+        Login.innerText = "लॉगिन";
+        SignUp.innerText = "साइन अप";
+    }
+} 
+///333333  marathi
+
+let marathi = document.querySelector("#mylink3");
+let marathisubtitle = document.querySelector(".subtitle");
+let marathidescription = document.querySelector(".description");
+let mbtn = document.querySelector("#Home");
+let mbtn2 = document.querySelector("#Dashboard"); 
+let mbtn3 = document.querySelector("#pc");
+let mbtn4 = document.querySelector("#Schemes");
+let mbtn5 = document.querySelector("#dd");
+let mbtn6 = document.querySelector("#Grievance");
+let mbtn7 = document.querySelector("Login");
+let mbtn8 = document.querySelector("#Sign Up");
+
+
+
+function change3 ()  {
+    mylink3.onclick = () => {
+        subtitle.innerText = "क्षमतांची उड्डाण, प्रत्येक खेळाडूसोबत";
+        marathidescription.innerText = "Athlence हे एक स्मार्ट ॲथलीट व्यवस्थापन व्यासपीठ आहे, जे ॲथलीट्स, प्रशिक्षक आणि प्रायोजकांना जोडते. हे कामगिरी ट्रॅक करण्यास, करिअर व्यवस्थापित करण्यास आणि क्रीडा क्षेत्रात नवीन संधी उघडण्यास मदत करते. आधुनिक तंत्रज्ञानाच्या मदतीने, आम्ही ॲथलीट्सना त्यांची पूर्ण क्षमता गाठण्यासाठी सक्षम करतो."
+        Home.innerText = "मुख्य पृष्ठ";
+        Dashboard.innerText = "संचालन फलक";
+        pc.innerText = "खासगी प्रशिक्षक";
+        Schemes.innerText = "योजना";
+        dd.innerText = "सहकार्य ";
+        Grievance.innerText = "तक्रार";
+        Login.innerText = "लॉगिन करा";
+        SignUp.innerText = "साइन अप करा";
+    }
+} 
